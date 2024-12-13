4f5c834bc0e60a13ce28e9855aa190495bef1903
Robert <rassawyer@gmail.com>
2024-12-13 09:44:31 -0500

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/aimJIdkRGP

---
+++ src/lib/shortcuts.spec.ts
@@ -0,0 +1,7 @@
+import { shortcuts } from './shortcuts';
+
+describe('shortcuts', () => {
+  it('should work', () => {
+    expect(shortcuts()).toEqual('Shortcuts');
+  });
+});
---
+++ src/lib/shortcuts.ts
@@ -0,0 +1,3 @@
+export function shortcuts(): string {
+  return 'Shortcuts';
+}
