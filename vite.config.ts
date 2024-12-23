92e69d8d0a6a0a7f9cf6346fb647d0f0cbb41ef5
Robert <rassawyer@gmail.com>
2024-12-23 15:15:52 -0500

    Setup: Initial commit after Svelte/NX/PNPM setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
 
 export default defineConfig({
   root: __dirname,
